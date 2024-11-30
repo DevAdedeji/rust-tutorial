85cdc647e0802645ecaa06ad5fd1f85175deb7b8
DevAdedeji <adedejitewogbade2@gmail.com>
2024-11-30 01:28:04 +0100

    Learnt about ownership and references

---
+++ src/ownership.rs
@@ -0,0 +1,19 @@
+fn main() {
+    // let s1 = String::from("RUST");
+    // let len = calculate_length(&s1);
+    // println!("{}", len)
+
+    // let x: i64 = 56;
+    // let y = x;
+    // println!("{}", x);
+    // println!("{}", y);
+
+    // let mut s1 = String::from("Hello");
+    // let s2 = &mut s1;
+    // s2.push_str("Rust");
+    // println!("{}", s2);
+}
+
+// fn calculate_length(s: &String) -> usize {
+//     s.len()
+// }
