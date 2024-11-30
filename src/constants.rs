38f348b6fc77f123bbce02de946e1683cb547e3a
DevAdedeji <adedejitewogbade2@gmail.com>
2024-11-30 02:25:27 +0100

    Learnt about constants

---
+++ src/constants.rs
@@ -0,0 +1,12 @@
+fn main() {
+    const Y: i32 = 10;
+    println!("The value of Y is: {}", Y);
+    println!("The value of pi is: {}", PI);
+    println!(
+        "The value of THREE_HOURS_IN_SECONDS is: {}",
+        THREE_HOURS_IN_SECONDS
+    );
+}
+
+const PI: f64 = 3.141592653;
+const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
