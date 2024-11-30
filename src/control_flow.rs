6073f7e9c9d2f2d9ce75cbb777d120db2c6f9957
DevAdedeji <adedejitewogbade2@gmail.com>
2024-11-30 03:12:49 +0100

    Learnt about control flow

---
+++ src/control_flow.rs
@@ -0,0 +1,41 @@
+fn main() {
+    // Conditional Statements
+    // let age: u64 = 18;
+    // let is_adult: bool = age >= 18;
+    // let is_adult = if age >= 18 {
+    //     "Is an adult"
+    // } else {
+    //     "Isn't an adult"
+    // };
+    // println!("{}", is_adult);
+
+    // Loops
+    // let mut counter = 0;
+
+    // let result = loop {
+    //     counter += 1;
+
+    //     if counter == 10 {
+    //         break counter * 2;
+    //     }
+    // };
+    // println!("{}", result);
+
+    // let mut number = 0;
+    // while number != 10 {
+    //     println!("Number is: {}", number);
+    //     number += 1
+    // }
+
+    // let numbers = [10, 20, 30, 40, 50];
+
+    // for number in numbers {
+    //     println!("{}", number);
+    // }
+
+    // let mut index = 0;
+    // while index < numbers.len() {
+    //     println!("Number is {}", numbers[index]);
+    //     index += 1;
+    // }
+}
