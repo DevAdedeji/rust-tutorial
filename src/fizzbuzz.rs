3b9f31bebfeef87ec0429b3045690055446874c2
DevAdedeji <adedejitewogbade2@gmail.com>
2024-12-04 13:20:25 +0100

    Did some refactoring

---
+++ src/fizzbuzz.rs
@@ -0,0 +1,16 @@
+fn main() {
+    let mut number = 1;
+
+    while number < 101 {
+        if number % 3 == 0 && number % 5 == 0 {
+            println!("FizzBuzz");
+        } else if number % 3 == 0 {
+            println!("Fizz");
+        } else if number % 5 == 0 {
+            println!("Buzz");
+        } else {
+            println!("{}", number);
+        }
+        number += 1;
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,3 @@
 fn main() {
-    // enum Option<T> {
-    //     Some(T),
-    //     None,
-    // }
-
-    // fn divide(num: f64, den: f64) -> Option<f64> {
-    //     if den == 0.0 {
-    //         None
-    //     } else {
-    //         Some(num / den)
-    //     }
-    // }
-
-    // let result = divide(10.0, 1.0);
-    // match result {
-    //     Some(x) => println!("Result: {}", x),
-    //     None => println!("Error"),
-    // };
-
-    // enum Result<T, E> {
-    //     Ok(T),
-    //     Err(E),
-    // }
-
-    fn divide(num: f64, den: f64) -> Result<f64, String> {
-        if den == 0.0 {
-            Err("Cannot divide by Zero".to_string())
-        } else {
-            Ok(num / den)
-        }
-    }
-    match divide(10.0, 0.0) {
-        Ok(result) => println!("Result is: {}", result),
-        Err(err) => println!("Error: {}", err),
-    }
+    //
 }
